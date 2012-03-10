7106085216549034f4c395782402adfcecf6bcbf
Mo McRoberts <mo@nevali.net>
2012-03-10 16:18:42 +0000

    Release 1.3.1

--- version.h
+++ version.h
@@ -1,3 +1,3 @@
-#define VERSION		"1.3"
-#define RELDATE		"1997-10-20"
-#define BUGREPADDR	"snacc@cs.ubc.ca"
+#define VERSION		"1.3.1"
+#define RELDATE		"2012-03-10"
+#define BUGREPADDR	"mo@nevali.net"
